3412a8010e9c24a6d21a0c79f71ef06a291359aa
Harpreet Kaur <harpreet.kaur9208@gmail.com>
2024-08-02 14:30:03 -0400

    changed link for img in project.js

--- src/Data/data.js
+++ src/Data/data.js
@@ -1,20 +1,14 @@
-const todo = process.env.PUBLIC_URL + '/images/projectImages/todo.png';
-const shoppingCart = process.env.PUBLIC_URL + '/images/shopp.png';
-const ecomm = process.env.PUBLIC_URL + '/images/projectImages/mobile.jpeg';
-const tictactoe = process.env.PUBLIC_URL + '/images/projectImages/tictac.jpeg';
-
-
 export const Project=[
   {  
     id:1,
     name:"Todo list Project",
-    img:todo,
+    img:'/images/projectImages/todo.png',
     gitLink:'https://hkaur108.github.io/todolist/'
   },
     {  
     id:2,
     name:"E-commerce Website",
-    img:ecomm,
+    img:'/images/projectImages/mobile.jpeg',
     gitLink:'https://hkaur108.github.io/e-commerce/'
 
   },
@@ -22,13 +16,13 @@ export const Project=[
     {  
     id:3,
     name:"Shopping Cart Website",
-    img:shoppingCart,
+    img:'/images/shopp.png',
     gitLink:'https://hkaur108.github.io/ShoppingCart/'
 },
  {  
     id:4,
     name:"Tic Tac Toe Game",
-    img:tictactoe,
+    img:'/images/projectImages/tictac.jpeg',
     gitLink:'https://hkaur108.github.io/tictactoe/'
 },
 //  {  
